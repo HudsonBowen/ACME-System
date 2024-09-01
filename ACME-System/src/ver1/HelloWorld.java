d07eb4b78812d9d08ed5be811ed355ad87b8be08
Hudson Bowen <HudsonTB3@gmail.com>
2024-09-01 02:13:38 -0400

    dsfs

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -7,5 +7,11 @@ public class HelloWorld {
 		System.out.println("Dave is working locally on his branch");
 
 	}
+	
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
+
 
 } //https://github.com/HudsonBowen/Acme-System.git
